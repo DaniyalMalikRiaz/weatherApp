d8ccaeac0642d96ff313beefd65324dcedafadf2
Daniyal Riaz Malik <daniyalmalikriaz@gmail.com>
2022-05-19 12:35:34 +0500

    Update BusinessController.js

--- BusinessController.js
+++ BusinessController.js
@@ -14,25 +14,26 @@ define([], function(){
  BusinessController.prototype.getWeatherAPI=function(data,presentationSuccessCallback,presentationErrorCallback){
       try{
         alert("call BusinessController");
-        alert(data);
-//       let KNYMobileFabric=kony.sdk.getCurrentInstance();
-//       let serviceName = BBConstants.WEATHER_API;
-//       let operationName = BBConstants.GET_CITY_WEATHER;
-//       let headers= {};
-//       let integrationObj = KNYMobileFabric.getIntegrationService(serviceName);
-
-//       integrationObj.invokeOperation(operationName, headers, data, operationSuccess, operationFailure);
+        //alert(data);
+      let KNYMobileFabric=kony.sdk.getCurrentInstance();
+      let serviceName = "Weather";
+      let operationName =  "getWeather";
+      let headers= {};
+      let integrationObj = KNYMobileFabric.getIntegrationService(serviceName);
+	
+      integrationObj.invokeOperation(operationName, headers, data, operationSuccess, operationFailure);
       }catch(e){
       alert("Error getWeatherAPI"+JSON.stringify(e));
       }//end of the catch body
-//       function operationSuccess(res){
-//       presentationSuccessCallback(res);
-//       }//end of the body operationSuccess
+      function operationSuccess(res){
+        alert("success");
+      presentationSuccessCallback(res);
+      }//end of the body operationSuccess
 
-//        function operationFailure(res){
-//       alert("operationFailure :"+JSON.stringify(res));
-//       presentationErrorCallback(res);
-//       }//end of the body operationFailure
+       function operationFailure(res){
+      alert("operationFailure :"+JSON.stringify(res));
+      presentationErrorCallback(res);
+      }//end of the body operationFailure
 
       };//end of the getHelloWorld block body
 return BusinessController;
